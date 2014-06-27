61884b051d28e891685c740af18c2be97bd88a27
Ritik Khatwani <rkhatwani@homeaway.com>
2014-06-27 16:03:13 +0530

    added moved map to global scope

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,19 +1,29 @@
+var map;
+
 angular.module('starter.directives', [])
 
 .directive('map', function() {
   return {
     restrict: 'E',
+    replace: true,
     scope: {
       onCreate: '&'
     },
     link: function($scope, $element, $attr) {
-      function initialize() {
-        var mapOptions = {
+      var mapOptions = {
           center: new google.maps.LatLng(18.61579, 73.911238),
           zoom: 15,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        var map = new google.maps.Map($element[0], mapOptions);
+        
+      if(map) {
+        $scope.onCreate({
+          map:map
+        });
+      }
+      
+      function initialize() {
+        map = new google.maps.Map($element[0], mapOptions);
 
         $scope.onCreate({
           map: map
