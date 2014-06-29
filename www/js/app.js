690fca8e4b1e8e8b8886a202690f4686c0dc6545
Ritik <ritik@ritik-HP-Mini-110-3500.(none)>
2014-06-29 06:28:12 +0530

    Added scroll disable for keyboard show

--- www/js/app.js
+++ www/js/app.js
@@ -6,6 +6,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives',
 		// for form inputs)
 		if(window.cordova && window.cordova.plugins.Keyboard) {
 			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			cordova.plugins.Keyboard.disableScroll(true)
 		}
 		if(window.StatusBar) {
 			// org.apache.cordova.statusbar required
